ab33e124e2872ba97d0793b729b0efd054fb003a
root <root@localhost.librenet>
2016-08-22 21:44:03 +0000

    modified easyrtc configuration

--- app-configuration-script.sh
+++ app-configuration-script.sh
@@ -945,6 +945,35 @@ if [ ! -e /opt/easyrtc/server.js ]; then
     exit 4
 fi
 
+cat << EOF > /opt/easyrtc/server.js 
+    // Load required modules
+    var https   = require("https");     // https server core module
+    var fs      = require("fs");        // file system core module
+    var express = require("express");   // web framework external module
+    var io      = require("socket.io"); // web socket external module
+    var easyrtc = require("easyrtc");   // EasyRTC external module
+
+    // Setup and configure Express http server. Expect a subfolder called "static" to be the web root.
+    var httpApp = express();
+    httpApp.use(express.static(__dirname + "/static/"));
+
+    // Start Express https server on port 8443
+    var webServer = https.createServer(
+    {
+        key:  fs.readFileSync("/etc/ssl/nginx/easyrtc.key"),
+        cert: fs.readFileSync("/etc/ssl/nginx/easyrtc.crt")
+    },
+    httpApp).listen(8443);
+
+    // Start Socket.io so it attaches itself to Express server
+    var socketServer = io.listen(webServer, {"log level":1});
+
+    // Start EasyRTC server
+    var rtc = easyrtc.listen(httpApp, socketServer);
+EOF
+
+sed -i '/function connect() {/a easyrtc.setSocketUrl(":8443");' /opt/easyrtc/node_modules/easyrtc/demos/js/*.js
+
 cd /opt/easyrtc
 
 # Starting EasyRTC server
@@ -2151,7 +2180,7 @@ server {
         ssl_certificate /etc/ssl/nginx/easyrtc.crt;
         ssl_certificate_key /etc/ssl/nginx/easyrtc.key;
   location / {
-    proxy_pass       http://127.0.0.1:8080;
+    proxy_pass       https://127.0.0.1:8443;
     proxy_set_header Host      \$host;
     proxy_set_header X-Real-IP \$remote_addr;
   }
@@ -2163,7 +2192,7 @@ server {
         ssl_certificate /etc/ssl/nginx/easyrtc.crt;
         ssl_certificate_key /etc/ssl/nginx/easyrtc.key;
   location / {
-    proxy_pass       http://127.0.0.1:8080;
+    proxy_pass       https://127.0.0.1:8443;
     proxy_set_header Host      \$host;
     proxy_set_header X-Real-IP \$remote_addr;
   }
