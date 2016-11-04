f5bd0bde178800d7edd70837f6baff0d3eecef25
nikdavnik <nikdavnik@gmail.com>
2016-11-04 17:08:49 -0400

    Added setup.sh

---
+++ setup.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+# -----------------------------------------------
+# Installation part
+# -----------------------------------------------
+
+echo "Downloading installation script ..."
+wget --no-check-certificate https://raw.githubusercontent.com/Librerouter/Librekernel/gh-pages/app-installation-script.sh
+if [ $? -ne 0 ]; then
+	echo "Unable to download installtaion script. Exiting ..."
+	exit 1
+fi
+echo "Running installation scirpt"
+chmod +x app-installation-script.sh
+app-installation-script.sh
+
+
+# -----------------------------------------------
+# Configuration part
+# -----------------------------------------------
+
+echo "Downloading configuration script ..."
+wget --no-check-certificate https://raw.githubusercontent.com/Librerouter/Librekernel/gh-pages/app-configuration-script.sh
+if [ $? -ne 0 ]; then
+        echo "Unable to download configuration script. Exiting ..."
+        exit 1
+fi
+echo "Runngin configuraiton script"
+chmod +x app-configuration-script.sh
+app-configuration-script.sh
