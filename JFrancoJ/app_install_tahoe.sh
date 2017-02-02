83011e85956d288aec4981373c3bb388e989b1e4
JFrancoJ <jfranco@circuitosaljarafe.com>
2017-02-02 02:35:45 +0000

    Add files via upload

--- JFrancoJ/app_install_tahoe.sh
+++ JFrancoJ/app_install_tahoe.sh
@@ -12,6 +12,22 @@
     venv/bin/pip install tahoe-lafs[tor,i2p] # required with TOR + I2P
 #fi
 
+
+# VERY IMPORTANT. FIX a bug that causes max file uploadaed through sshfs/sftp 
+# comes to zero size if size > 55 bytes. 
+# This happens some times ( 50% aprox repeatable ) not only on inmutable but also on mutables
+
+# The fix is /home/tahoe-lafs/venv/lib/python2.7/site-packages/allmydata/immutable/upload.py file line 1519 must:
+# -URI_LIT_SIZE_THRESHOLD = 55
+# +URI_LIT_SIZE_THRESHOLD = 55555 
+# cd /home/tahoe-lafs/venv/lib/python2.7/site-packages/allmydata/immutable; python -m py_compile upload.py
+# 
+patching=$(sed -e "s/URI_LIT_SIZE_THRESHOLD = 55/URI_LIT_SIZE_THRESHOLD = 55555/g" upload.py > /tmp/upload.py.patched)
+mv /tmp/upload.py.patched /home/tahoe-lafs/venv/lib/python2.7/site-packages/allmydata/immutable/upload.py
+cd /home/tahoe-lafs/venv/lib/python2.7/site-packages/allmydata/immutable
+python -m py_compile upload.py
+
+
 # /usr/node_1 is used to store PRIVATE Tahoe node_1
 # /usr/private_node is used to store ( shared ) common PUBLIC Tahoe node
 # /var/node_1 is a mount point for node_1 FUSE
